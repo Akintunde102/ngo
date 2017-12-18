18cc5ace2f9c334c2d2aa4147899e7c1e9055307
Akintunde102 <jegedeakintunde@gmail.com>
2017-12-18 20:37:22 +0100

    Initial commit

---
+++ js/search.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+
+// Google Internal Site Search script- By JavaScriptKit.com (http://www.javascriptkit.com)
+// For this and over 400+ free scripts, visit JavaScript Kit- http://www.javascriptkit.com/
+// This notice must stay intact for use
+
+//Enter domain of site to search.
+var domainroot="www.upcawa.org"
+
+function Gsitesearch(curobj){
+curobj.q.value="site:"+domainroot+" "+curobj.qfront.value
+}
+
+</script>
